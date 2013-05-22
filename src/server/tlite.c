c1a500a827a5ad41f1682660b075a29696ebda2c
Andrei Kholodnyi <Andrei.Kholodnyi@gmail.com>
2013-05-22 11:53:24 +0200

    added --help

--- src/server/tlite.c
+++ src/server/tlite.c
@@ -11,8 +11,25 @@
 static TLiteCeDeviceManager *manager;
 static GMainLoop *main_loop;
 
+static GOptionEntry options[] =
+{
+	{ NULL }
+};
+
 int main(int argc, char *argv[])
 {
+	GOptionContext *context;
+	GError *error = NULL;
+	gboolean retval;
+
+	context = g_option_context_new ("- indexer of multimedia devices");
+	g_option_context_set_summary (context,
+                                "Example: to index directory, use:\n"
+                                "\n"
+                                "  ./tlite\n"
+                                "  mount-added <dirname>");
+	retval = g_option_context_parse (context, &argc, &argv, &error);
+	g_option_context_free (context);
 
 	main_loop = g_main_loop_new (NULL, FALSE);
 	manager = tlite_ce_device_manager_new ();
